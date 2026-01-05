a8a58eef3f300d8943dbf3025f89d0afebbeef51
r1zzertt <danil16gg4@gmail.com>
2026-01-06 02:42:52 +0700

    Update state.py

--- state.py
+++ state.py
@@ -4,14 +4,16 @@ def reset_user(user_id: int):
     users[user_id] = {
         "node": "start",
         "inventory": [],
-        "wait_voice": None
+        "wait_voice": None,
+        "flags": {}
     }
 
 def get_user_state(user_id: int):
     return users.setdefault(user_id, {
         "node": "start",
         "inventory": [],
-        "wait_voice": None
+        "wait_voice": None,
+        "flags": {}
     })
 
 def wait_for_voice(user, key):
