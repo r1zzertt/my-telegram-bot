f22ac9bfbbd243d15626c7ad36a102a183abcf76
r1zzertt <danil16gg4@gmail.com>
2026-01-06 16:37:59 +0700

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -6,3 +6,9 @@ def inventory_text(user):
     if not user["inventory"]:
         return "🎒 Инвентарь пуст."
     return "🎒 Инвентарь:\n\n" + "\n".join(f"• {item}" for item in user["inventory"])
+
+def remove_item(user, item):
+    if item in user.get("inventory", []):
+        user["inventory"].remove(item)
+        return True
+    return False
